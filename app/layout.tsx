4ccfe67b77ae4a84ee5ca380a91471f43100774d
oliv37 <olivier.boisse@laposte.net>
2024-02-15 22:07:16 +0100

    rename to ablar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jafare Geo",
+  title: "Ablar Geo",
   description: "Geography at localhost",
 };
 
